c2085faf8f581da9e33151bd7f0cb109d42c9b14
Younghwan Lee <130021636+lyh0105@users.noreply.github.com>
2023-12-23 10:05:31 +0900

    Update mlp.py

--- main/MLP/mlp.py
+++ main/MLP/mlp.py
@@ -1,107 +1,112 @@
 import pandas as pd
+import os
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
-# 데이터 로딩 및 전처리
-def load_data(train_path, valid_path, test_path):
-    train_df = pd.read_csv(train_path)
-    valid_df = pd.read_csv(valid_path)
-    test_df = pd.read_csv(test_path)
-    
-    X_train = train_df.iloc[:, :-1].values
-    y_train = train_df.iloc[:, -1].values.reshape(-1, 1)
-    X_valid = valid_df.iloc[:, :-1].values
-    y_valid = valid_df.iloc[:, -1].values.reshape(-1, 1)
-    X_test = test_df.iloc[:, :-1].values
-    y_test = test_df.iloc[:, -1].values.reshape(-1, 1)
-    
-    # 데이터를 PyTorch 텐서로 변환
-    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
-    X_valid_tensor = torch.tensor(X_valid, dtype=torch.float32)
-    y_valid_tensor = torch.tensor(y_valid, dtype=torch.float32)
-    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
-    
-    return X_train_tensor, y_train_tensor, X_valid_tensor, y_valid_tensor, X_test_tensor, y_test_tensor
+# Function to load data from CSV and process it
+def load_and_process_data(file_name):
+    df = pd.read_csv(file_name)
+    features = df.iloc[:, 1:-1].to_numpy()
+    labels = df.iloc[:, -1].to_numpy()
 
-# MLP 모델 클래스
+    features_tensor = torch.tensor(features, dtype=torch.float32)
+    labels_tensor = torch.tensor(labels, dtype=torch.float32).view(-1, 1)
+
+    return features_tensor, labels_tensor
+
+
+# Load the datasets
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_folder = 'data_reconstructed'
+
+X_train, y_train = load_and_process_data(os.path.join(script_dir, data_folder, 'train_False_df2.csv'))
+X_val, y_val = load_and_process_data(os.path.join(script_dir, data_folder, 'valid_False_df2.csv'))
+X_test, y_test = load_and_process_data(os.path.join(script_dir, data_folder, 'test_df2.csv'))
+
+train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=64, shuffle=False)
+val_loader = DataLoader(TensorDataset(X_val, y_val), batch_size=64)
+test_loader = DataLoader(TensorDataset(X_test, y_test), batch_size=64)
+
+n_features = X_train.shape[1] 
+n_targets = 1
+
+
 class MLPRegressor(nn.Module):
-    def __init__(self, input_size, hidden_size=64, output_size=1):
+    def __init__(self, n_features, n_targets, num_layers, hidden_size, dropout_rate=0):
         super(MLPRegressor, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu1 = nn.ReLU()
+        layers = [nn.Linear(n_features, hidden_size), nn.ReLU()]
         
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.relu2 = nn.ReLU()
-        
-        self.fc3 = nn.Linear(hidden_size, hidden_size)
-        self.relu3 = nn.ReLU()
-        
-        self.fc4 = nn.Linear(hidden_size, output_size)
+        for _ in range(num_layers - 1):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout_rate))
+
+        layers.append(nn.Linear(hidden_size, n_targets))
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        
-        x = self.fc2(x)
-        x = self.relu2(x)
-        
-        x = self.fc3(x)
-        x = self.relu3(x)
-        
-        x = self.fc4(x)
-        return x
+        return self.layers(x)
 
-# 모델 훈련 함수
-def train_model(model, train_loader, valid_loader, criterion, optimizer, scheduler, num_epochs=100):
-    for epoch in range(num_epochs):
-        model.train()
-        total_loss = 0
-        for inputs, targets in train_loader:
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        
-        # 스케줄러 스텝
-        scheduler.step()
-        
-        # Validate the model
-        model.eval()
-        with torch.no_grad():
-            valid_loss = sum(criterion(model(inputs), targets) for inputs, targets in valid_loader)
-        
-        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/len(train_loader)}, Valid Loss: {valid_loss/len(valid_loader)}')
+
+# Example of model instantiation with different layer sizes
+num_layers = 3
+hidden_size = 128
+model = MLPRegressor(n_features, n_targets, num_layers, hidden_size, dropout_rate=0)
+
+
+# Define loss function and optimizer
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
+
+# Training loop
+def train(dataloader, model, loss_fn, optimizer):
+    model.train()
+    total_loss = 0
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        total_loss += loss.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
     
-    print("Training complete")
+    avg_loss = total_loss / len(dataloader)
+    return avg_loss
 
-# 데이터 파일 경로
-train_path = './train_False_df.csv'
-valid_path = './valid_False_df.csv' 
-test_path = './test_df.csv'
+# Validation function
+def validate(dataloader, model, loss_fn):
+    model.eval()
+    total_val_loss = 0
+    total_val_mape = 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            loss = loss_fn(pred, y)
+            total_val_loss += loss.item()
+            total_val_mape += mean_absolute_percentage_error(y.numpy(), pred.numpy())
+            
+    avg_val_loss = total_val_loss / len(dataloader)
+    avg_val_mape = total_val_mape / len(dataloader)
+    return avg_val_loss, avg_val_mape
 
-# 데이터 로드
-X_train, y_train, X_valid, y_valid, X_test, y_test = load_data(train_path, valid_path, test_path)
+# Train and validate the model
+epochs = 201
+for epoch in range(epochs):
+    train_loss = train(train_loader, model, loss_fn, optimizer)
+    val_loss, val_mape = validate(val_loader, model, loss_fn)
+    if epoch % 1 == 0:  # Print every 10 epochs
+        print(f"Epoch {epoch+1}: Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val MAPE: {val_mape:.4f}")
 
-# DataLoader 설정
-batch_size = 128
-train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size, shuffle=False)
-valid_loader = DataLoader(TensorDataset(X_valid, y_valid), batch_size=batch_size)
-test_loader = DataLoader(TensorDataset(X_test, y_test), batch_size=batch_size)
+# Evaluate the model on the test set
+model.eval()
+with torch.no_grad():
+    y_pred = model(X_test)
+    test_loss = loss_fn(y_pred, y_test).item()
+    test_mape = mean_absolute_percentage_error(y_test.numpy(), y_pred.numpy())
 
-# 모델 초기화
-input_size = X_train.shape[1]
-model = MLPRegressor(input_size)
-
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)
-
-# 모델 훈련
-train_model(model, train_loader, valid_loader, criterion, optimizer, scheduler, num_epochs=300)
+print(f"Test Loss: {test_loss:.4f}")
+print(f"Test MAPE: {test_mape:.4f}")
